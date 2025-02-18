d86f5399cc7fa25b5163c354afdd073e8081f648
thedorknight <huy.hoang@alumni.vcu.edu>
2025-02-18 18:03:19 -0500

    Updated for 2025
    Adjusted flow of overall script. Adjusted for new amazon new login flow. Added duplicate logic.

--- bookp.py
+++ bookp.py
@@ -12,50 +12,103 @@ import urllib.parse
 from argparse import ArgumentParser
 from pyvirtualdisplay import Display
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 
 user_agent = {'User-Agent': 'krumpli'}
 logger = logging.getLogger(__name__)
 
-def create_session(email, password, browser_visible=False, proxy=None):
+
+def create_session(backup_email=None, backup_password=None, backup_oath=None, browser_visible=True, proxy=None):
+    # Only start a virtual display if browser_visible is False (i.e. headless mode)
     if not browser_visible:
+        from pyvirtualdisplay import Display
         display = Display(visible=0)
         display.start()
 
     logger.info("Starting browser")
     options = webdriver.ChromeOptions()
     if proxy:
-        options.add_argument('--proxy-server='+proxy)
-    browser = webdriver.Chrome(chrome_options=options)
+        options.add_argument('--proxy-server=' + proxy)
+    # If browser_visible is True, we do not add the headless option.
+    if not browser_visible:
+        options.add_argument('--headless')
+    browser = webdriver.Chrome(options=options)
 
-    logger.info("Loading www.amazon.com")
-    browser.get('https://www.amazon.com')
+    # --- Manual Login Phase ---
+    logger.info("Opening Amazon Sign-In page for manual login")
+    # Updated URL for manual login:
+    browser.get('https://www.amazon.com/gp/sign-in.html')
+    print("Please log in manually in the opened browser window (complete MFA if required).")
+    input("After you've logged in, press Enter to continue...")
 
-    logger.info("Logging in")
-    browser.find_element_by_css_selector("#nav-signin-tooltip > a.nav-action-button").click()
-    browser.find_element_by_id("ap_email").clear()
-    browser.find_element_by_id("ap_email").send_keys(email)
-
-    browser.find_element_by_id("ap_password").clear()
-    browser.find_element_by_id("ap_password").send_keys(password)
-    browser.find_element_by_id("signInSubmit").click()
-
-    logger.info("Getting CSRF token")
-    browser.get('https://www.amazon.com/hz/mycd/myx#/home/content/booksAll')
-
+    logger.info("Navigating to digital console page to retrieve session data")
+    browser.get('https://www.amazon.com/hz/mycd/digital-console/contentlist/booksAll/dateDsc/')
+    
+    # Attempt to extract the CSRF token and customer ID
+    csrf_token = None
     match = re.search('var csrfToken = "(.*)";', browser.page_source)
     if match:
         csrf_token = match.group(1)
 
+    custid = None
+    match = re.search('customerId: \"(.*)\"', browser.page_source)
+    if match:
+        custid = match.group(1)
+
+    # --- Fallback: Automated Login if Manual Login Fails ---
+    if not csrf_token:
+        print("Manual login did not seem to complete successfully.")
+        answer = input("Would you like to attempt automated login with backup credentials? (y/n): ")
+        if answer.strip().lower().startswith('y'):
+            # If backup credentials weren't provided already, prompt for them now.
+            if not (backup_email and backup_password and backup_oath):
+                backup_email = input("Enter your Amazon email for automated login: ")
+                backup_password = getpass.getpass("Enter your Amazon password for automated login: ")
+                backup_oath = getpass.getpass("Enter your Amazon Oath for automated login: ")
+            try:
+                logger.info("Attempting automated login using backup credentials.")
+                browser.get('https://www.amazon.com/gp/sign-in.html')
+                browser.find_element(By.ID, "ap_email").clear()
+                browser.find_element(By.ID, "ap_email").send_keys(backup_email)
+                browser.find_element(By.CSS_SELECTOR, '.a-button-input').click()
+                browser.find_element(By.ID, "ap_password").clear()
+                browser.find_element(By.ID, "ap_password").send_keys(backup_password)
+                browser.find_element(By.ID, "signInSubmit").click()
+                browser.find_element(By.ID, "auth-mfa-otpcode").clear()
+                browser.find_element(By.ID, "auth-mfa-otpcode").send_keys(backup_oath)
+                browser.find_element(By.ID, "auth-signin-button").click()
+                # Navigate again to the digital console page.
+                browser.get('https://www.amazon.com/hz/mycd/digital-console/contentlist/booksAll/dateDsc/')
+                match = re.search('var csrfToken = "(.*)";', browser.page_source)
+                if match:
+                    csrf_token = match.group(1)
+                match = re.search('customerId: \"(.*)\"', browser.page_source)
+                if match:
+                    custid = match.group(1)
+            except Exception as e:
+                logger.error("Automated login failed: " + str(e))
+                browser.quit()
+                if not browser_visible:
+                    display.stop()
+                sys.exit(1)
+        else:
+            print("Exiting due to failed manual login.")
+            browser.quit()
+            if not browser_visible:
+                display.stop()
+            sys.exit(1)
+
+    # Grab cookies from the session
     cookies = {}
     for cookie in browser.get_cookies():
         cookies[cookie['name']] = cookie['value']
 
     browser.quit()
     if not browser_visible:
-        display.stop();
+        display.stop()
 
-    return cookies, csrf_token
+    return cookies, csrf_token, custid
+
 
 """
 NOTE: This function is not used currently, because the download URL can be
@@ -81,31 +134,33 @@ def get_download_url(user_agent, cookies, csrf_token, asin, device_id):
     return rr["URL"] if rr["success"] else None
 """
 
+
 def get_devices(user_agent, cookies, csrf_token):
     logger.info("Getting device list")
     data_json = {'param': {'GetDevices': {}}}
-    
+
     r = requests.post('https://www.amazon.com/hz/mycd/ajax',
-        data={'data':json.dumps(data_json), 'csrfToken':csrf_token},
-        headers=user_agent, cookies=cookies)
+                      data={'data': json.dumps(data_json), 'csrfToken': csrf_token},
+                      headers=user_agent, cookies=cookies)
     devices = json.loads(r.text)["GetDevices"]["devices"]
 
     return [device for device in devices if 'deviceSerialNumber' in device]
+
 
 def get_asins(user_agent, cookies, csrf_token):
     logger.info("Getting e-book list")
     startIndex = 0
     batchSize = 100
     data_json = {
-        'param':{
-            'OwnershipData':{
-                'sortOrder':'DESCENDING',
-                'sortIndex':'DATE',
-                'startIndex':startIndex,
-                'batchSize':batchSize,
-                'contentType':'Ebook',
-                'itemStatus':['Active'],
-                'originType':['Purchase'],
+        'param': {
+            'OwnershipData': {
+                'sortOrder': 'DESCENDING',
+                'sortIndex': 'DATE',
+                'startIndex': startIndex,
+                'batchSize': batchSize,
+                'contentType': 'Ebook',
+                'itemStatus': ['Active'],
+                'originType': ['Purchase'],
             }
         }
     }
@@ -117,8 +172,8 @@ def get_asins(user_agent, cookies, csrf_token):
     asins = []
     while True:
         r = requests.post('https://www.amazon.com/hz/mycd/ajax',
-            data={'data':json.dumps(data_json), 'csrfToken':csrf_token},
-            headers=user_agent, cookies=cookies)
+                          data={'data': json.dumps(data_json), 'csrfToken': csrf_token},
+                          headers=user_agent, cookies=cookies)
         rr = json.loads(r.text)
         asins += [book['asin'] for book in rr['OwnershipData']['items']]
 
@@ -130,18 +185,35 @@ def get_asins(user_agent, cookies, csrf_token):
 
     return asins
 
-def download_books(user_agent, cookies, device, asins, directory):
+
+def download_books(user_agent, cookies, device, asins, custid, directory):
     logger.info("Downloading {} books".format(len(asins)))
-    cdn_url = 'http://cde-g7g.amazon.com/FionaCDEServiceEngine/FSDownloadContent'
-    cdn_params = 'type=EBOK&key={}&fsn={}&device_type={}'
+    cdn_url = 'https://cde-ta-g7g.amazon.com/FionaCDEServiceEngine/FSDownloadContent'
+    cdn_params = 'type=EBOK&key={}&fsn={}&device_type={}&customerId={}&authPool=Amazon'
+
     for asin in asins:
         try:
-            params = cdn_params.format(asin, device['deviceSerialNumber'], device['deviceType'])
+            params = cdn_params.format(asin, device['deviceSerialNumber'], device['deviceType'], custid)
             r = requests.get(cdn_url, params=params, headers=user_agent, cookies=cookies, stream=True)
-            name = re.findall("filename\*=UTF-8''(.+)", r.headers['Content-Disposition'])[0]
+            
+            # Extract the filename from the Content-Disposition header
+            content_disp = r.headers.get('Content-Disposition', '')
+            match = re.findall("filename\\*=UTF-8''(.+)", content_disp)
+            if not match:
+                logger.error("Filename not found in headers for ASIN: " + asin)
+                continue
+            name = match[0]
             name = urllib.parse.unquote(name)
             name = name.replace('/', '_')
-            with open(os.path.join(directory, name), 'wb') as f:
+            
+            # Build the full path and check if file exists
+            file_path = os.path.join(directory, name)
+            if os.path.exists(file_path):
+                logger.info("Skipping {}: file already exists ({})".format(asin, name))
+                continue
+
+            # Download and save the file
+            with open(file_path, 'wb') as f:
                 for chunk in r.iter_content(chunk_size=512):
                     f.write(chunk)
             logger.info('Downloaded ' + asin + ': ' + name)
@@ -149,38 +221,48 @@ def download_books(user_agent, cookies, device, asins, directory):
             logger.debug(e)
             logger.error('Failed to download ' + asin)
 
+
 def main():
     parser = ArgumentParser(description="Amazon e-book downloader.")
-    parser.add_argument("--verbose", help="show info messages", action="store_true")
-    parser.add_argument("--showbrowser", help="display browser while creating session.", action="store_true")
-    parser.add_argument("--email", help="Amazon account e-mail address", required=True)
-    parser.add_argument("--password", help="Amazon account password", default=None)
-    parser.add_argument("--outputdir", help="download directory (default: books)", default="books")
+    # Set verbose to default True
+    parser.add_argument("--verbose", help="show info messages", action="store_true", default=True)
+    # Use --hidebrowser to run headless; by default, the browser is visible for manual login.
+    parser.add_argument("--hidebrowser", help="run browser in headless mode", action="store_true")
+    parser.add_argument("--outputdir", help="download directory (default: books)", default=None)
     parser.add_argument("--proxy", help="HTTP proxy server", default=None)
     parser.add_argument("--asin", help="list of ASINs to download", nargs='*')
+    parser.add_argument("--logfile", help="name of file to write log to", default=None)
     args = parser.parse_args()
 
-    if args.verbose:
-        logger.setLevel(logging.INFO)
-    else:
-        logger.setLevel(logging.WARNING)
+    # Verbose logging is now enabled by default
+    logger.setLevel(logging.INFO)
     formatter = logging.Formatter('[%(levelname)s]\t%(asctime)s %(message)s')
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     logger.addHandler(handler)
+    if args.logfile:
+        handlerLog = logging.FileHandler(args.logfile)
+        logger.addHandler(handlerLog)
 
-    password = args.password
-    if not password:
-        password = getpass.getpass("Your Amazon password: ")
-
-    if os.path.isfile(args.outputdir):
+    # Determine output directory:
+    if args.outputdir:
+        output_dir = args.outputdir
+    else:
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        output_dir = os.path.join(script_dir, "books")
+    if os.path.isfile(output_dir):
         logger.error("Output directory is a file!")
         return -1
-    elif not os.path.isdir(args.outputdir):
-        os.mkdir(args.outputdir)
+    elif not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
 
-    cookies, csrf_token = create_session(args.email, password,
-        browser_visible=args.showbrowser, proxy=args.proxy)
+    browser_visible = not args.hidebrowser
+
+    # No credentials are prompted for initially. Manual login is the default.
+    cookies, csrf_token, custid = create_session(
+        browser_visible=browser_visible, proxy=args.proxy
+    )
+    
     if not args.asin:
         asins = get_asins(user_agent, cookies, csrf_token)
     else:
@@ -198,15 +280,10 @@ def main():
         if choice in range(len(devices)):
             break
 
-    download_books(user_agent, cookies, devices[choice], asins, args.outputdir)
-
-    print("\n\nAll done!\nNow you can use apprenticeharper's DeDRM tools " \
-            "(https://github.com/apprenticeharper/DeDRM_tools)\n" \
-            "with the following serial number to remove DRM: " +
-            devices[choice]['deviceSerialNumber'])
+    download_books(user_agent, cookies, devices[choice], asins, custid, output_dir)
 
 if __name__ == '__main__':
     try:
-    	sys.exit(main())
+        sys.exit(main())
     except KeyboardInterrupt:
         logger.info("Exiting...")
